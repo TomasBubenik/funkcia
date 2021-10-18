0dbebca87abe44dd7bbf0e4ec1ade202d64cf54a
TomasBubenik <91113218+TomasBubenik@users.noreply.github.com>
2021-10-18 10:23:36 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+x=0
+def funkcia(x):
+    print("###############")
+    print("#Tomáš Bubeník#")
+    print("###############")
+    for i in range(10):
+        print(x)
+        x = x+1
+        
+
+funkcia(x)
